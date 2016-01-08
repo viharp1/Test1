72750278224aaa69b748b8e010629cbaf7dc1b69
viharp1 <viharp95@gmail.com>
2016-01-08 16:23:27 -0500

    SWWEEEET BACKGROUND

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Texture backgroundTexture;    
+
+
+    void OnGUI()
+    {
+        //Display backgroud
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+    }
+
+
+
+}
