91edd0d54e73de7146ffa604d56e26f3c4272ba4
viharp1 <viharp95@gmail.com>
2016-01-08 17:14:59 -0500

    Buttons

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -4,12 +4,33 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
     public Texture backgroundTexture;    
+    
 
 
     void OnGUI()
     {
         //Display backgroud
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        
+
+
+        //Display buttons
+        if (GUI.Button(new Rect(Screen.width * .33f, Screen.height * .5f, Screen.width * .33f, Screen.height * .1f), "Play Game"))
+        {
+            print("Clicked Play Game");
+        }
+
+        if (GUI.Button(new Rect(Screen.width * .33f, Screen.height * .7f, Screen.width * .33f, Screen.height * .1f), "Options"))
+        {
+            print("Clicked Options");
+        
+
+
+
+    }
+
+
     }
 
 
